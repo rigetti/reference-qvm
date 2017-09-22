34d5e72f8b8d2df4e33bd138c81ebd3e7ffece4a
Johannes Otterbach <johannesotterbach@gmail.com>
2017-09-22 16:55:56 -0700

    Make Arb State Gen not so Arb

--- referenceqvm/tests/test_arbitrary_state_prep.py
+++ referenceqvm/tests/test_arbitrary_state_prep.py
@@ -1,17 +1,20 @@
 import numpy as np
-from grove.arbitrary_state.arbitrary_state import create_arbitrary_state
-from tqdm import tqdm
+from referenceqvm.tests.test_data import data_containers as dc
+from pyquil.quil import Program
 
 
 def test_generate_arbitrary_states(qvm):
-    for length in tqdm(range(1, 2 ** 4 + 1)):
-        v = 10 * np.random.random() * (np.random.random(length) - 0.5) \
-          + 5j * (np.random.random(length) - 0.5)
+    for k in list(dc.ARBITRARY_STATE_GEN_INSTRUCTIONS.keys()):
+
+        v = np.asarray(dc.ARBITRARY_STATE_GEN_WF[k])
         norm = np.sqrt(np.sum(np.multiply(np.conj(v), v)))
-        p = create_arbitrary_state(v)
+
+        p = Program(dc.ARBITRARY_STATE_GEN_INSTRUCTIONS[k])
         wf, _ = qvm.wavefunction(p)
+
         # check actual part of wavefunction
-        assert np.allclose(v.reshape((-1, 1)), wf.amplitudes[:len(v),:] * norm)
+        assert np.allclose(v.reshape((-1, 1)), wf.amplitudes[:len(v), :] * norm)
+
         # check remaining zeros part of wavefunction
-        assert np.allclose(np.zeros((wf.amplitudes.shape[0] - len(v), 1)), \
+        assert np.allclose(np.zeros((wf.amplitudes.shape[0] - len(v), 1)),
                            wf.amplitudes[len(v):] * norm)
